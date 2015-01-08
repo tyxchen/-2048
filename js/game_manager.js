86993cb239cde55529565c8dcd768521e7009380
tyxchen <tyxchen@outlook.com>
2015-01-07 22:56:24 -0500

    Add negative numbers to the mix
    Sorry.

--- js/game_manager.js
+++ js/game_manager.js
@@ -154,7 +154,7 @@ GameManager.prototype.move = function (direction) {
 
         // Only one merger per row traversal?
         if (next && next.value === tile.value && !next.mergedFrom) {
-          var merged = new Tile(positions.next, tile.value * 2);
+          var merged = new Tile(positions.next, tile.value * (Math.random() < 0.5 ? -2 : 2)); // screw with everyone
           merged.mergedFrom = [tile, next];
 
           self.grid.insertTile(merged);
@@ -167,7 +167,7 @@ GameManager.prototype.move = function (direction) {
           self.score += merged.value;
 
           // The mighty 2048 tile
-          if (merged.value === 2048) self.won = true;
+          if (Math.abs(merged.value) === 2048) self.won = true;
         } else {
           self.moveTile(tile, positions.farthest);
         }
